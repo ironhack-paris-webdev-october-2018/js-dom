4d2db5fe97cabd01037a784aae4cf2846dbe781d
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-10-19 11:35:26 +0200

    Add finished comments example

--- js/post-script.js
+++ js/post-script.js
@@ -3,6 +3,8 @@ console.log("POST SCRIPT.JS connected");
 var postBtn = document.querySelector(".post-button");
 var commentList = document.querySelector("ol");
 var inputTag = document.querySelector("input");
+var deleteBtn = document.querySelectorAll(".delete");
+
 
 postBtn.onclick = function () {
   console.log("coucou post comment");
@@ -15,6 +17,37 @@ postBtn.onclick = function () {
   // add the new <li> to an element ALREADY in the DOM
   commentList.appendChild(newLi);
 
+  // add the click functionality to the new button
+  var newBtn = newLi.querySelector(".delete");
+  newBtn.onclick = function () {
+    console.log("coucou delete button");
+
+    // Confirm with the user that the delete is okay
+    var isOkay = confirm("Are you sure you want to delete this comment?");
+    if (isOkay) {
+      // get the PARENT of the <button> (the <li> tag that the <button> is inside)
+      var parentLi = newBtn.parentNode;
+      // remove the parent from the DOM
+      parentLi.remove();
+    }
+  };
+
   // reset the form after the click is done
   inputTag.value = "";
 };
+
+// add the click functionality to the buttons that are there AT FIRST
+deleteBtn.forEach(function (oneButton) {
+  oneButton.onclick = function () {
+    console.log("coucou delete button");
+
+    // Confirm with the user that the delete is okay
+    var isOkay = confirm("Are you sure you want to delete this comment?");
+    if (isOkay) {
+      // get the PARENT of the <button> (the <li> tag that the <button> is inside)
+      var parentLi = oneButton.parentNode;
+      // remove the parent from the DOM
+      parentLi.remove();
+    }
+  };
+});
